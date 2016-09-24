f48a3fa18929f32e9e4e2e73da23e2a12eca1aa5
ezzye <ezzy.elliott@gmail.com>
2016-09-24 15:00:54 +0100

    remove reference to source in sh file

--- supayuzamenyu.sh
+++ supayuzamenyu.sh
@@ -54,21 +54,6 @@ funcMenuLink() {
   sudo ln -s ~/Downloads/supayuzamenyu-master/menu .
 }
 
-# # derive the Crucial installation dir
-# THISWORKINGDIR="$(dirname $0)"
-
-# # require a runcom file that exists and is not empty and is not this file and
-# # is named ending *rc; if not defined then look for the sample runcom
-# [[ -s "${THISRCFILE:=$THISWORKINGDIR/supayuzamenyu.rc}" && \
-#   "$(readlink -f $THISRCFILE)" != "$(readlink -f $0)" && \
-#   "$THISRCFILE" =~ rc$ ]] || { 
-#     funcExit "$THISRCFILE is not a valid runcom file"; 
-# }
-
-
-set -o xtrace
-source $THISRCFILE
-set +o xtrace
 
 # Download crucial files and unzip
 funcDownload
