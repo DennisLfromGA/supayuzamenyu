a7027ffdbe4a2000dda5a353cdc4652ad8d25014
Errol Elliott <ezzy.elliott@gmail.com>
2016-10-11 13:35:51 +0100

    added install as root

--- target_files/eclipse.sh
+++ target_files/eclipse.sh
@@ -5,8 +5,7 @@
 wget -O "eclipse-inst-linux64.tar.gz" "http://eclipse.bluemix.net/packages/neon.1/data/eclipse-inst-linux64.tar.gz"
 tar xf eclipse-inst-linux64.tar.gz
 cd eclipse-installer
-echo "choose home directory to install"
 
 chmod a+x eclipse-inst
-./eclipse-inst
+sudo ./eclipse-inst
 
