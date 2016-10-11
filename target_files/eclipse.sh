e24d987fa50487c12a38c255930b10667d291114
Errol Elliott <ezzy.elliott@gmail.com>
2016-10-11 13:19:30 +0100

    updated url

--- target_files/eclipse.sh
+++ target_files/eclipse.sh
@@ -2,7 +2,7 @@
 
 # install eclipse
 
-wget -O "eclipse-inst-linux64.tar.gz" "https://www.eclipse.org/downloads/download.php?file=/oomph/epp/neon/R1/eclipse-inst-linux64.tar.gz&mirror_id=1190"
+wget -O "eclipse-inst-linux64.tar.gz" "http://eclipse.bluemix.net/packages/neon.1/data/eclipse-inst-linux64.tar.gz"
 tar xf eclipse-inst-linux64.tar.gz
 cd eclipse-installer
 echo "choose home directory to install"
